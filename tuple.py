b17abf5c506125bcc8ae1b85784ee4d73b2dd006
filfilt <tafneg4@gmail.com>
2020-08-06 06:30:44 -0700

    tuple Example

---
+++ tuple.py
@@ -0,0 +1,8 @@
+students=("nega","abebe","bekele","nega")
+#print(students)
+#print(students[0:2])
+#print(students[2])
+#print(students[-1])
+
+#students[0]="Eyos" # not allowed
+print(students[0])
