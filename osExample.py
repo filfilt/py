b94a3ff42dfe90c2219bca112306058fb534bf7d
filfilt <tafneg4@gmail.com>
2020-08-06 10:33:42 -0700

    Os example

---
+++ osExample.py
@@ -0,0 +1,9 @@
+#To create a folder
+import os
+os.mkdir('week3')
+# os.mkdir('week4')
+#os.makedirs('Student/Grade/11thGrade')
+
+#To delete a folder
+#os.rmdir('week4')
+#os.rmdir('Student/Grade/9thGrade')
