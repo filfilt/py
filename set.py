e93c177d482098823ae3c421b6f6b2dd657f07c8
filfilt <tafneg4@gmail.com>
2020-08-06 06:33:22 -0700

    Example of set

---
+++ set.py
@@ -0,0 +1,8 @@
+students={"nega","abebe","bekele","nega"} # duplicate not allowed
+#print(students)
+#print(students[0:2])
+#print(students[2])
+#print(students[-1])
+
+#students[0]="Eyos" 
+print(students)
