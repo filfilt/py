a070920a515020641e06856708df42f384cf5c44
filfilt <tafneg4@gmail.com>
2020-08-06 06:26:50 -0700

    another commit

--- listExample.py
+++ listExample.py
@@ -1,5 +1,8 @@
 students=["nega","abebe","bekele","nega"]
 #print(students)
 #print(students[0:2])
-print(students[2])
+#print(students[2])
 #print(students[-1])
+
+students[0]="Eyos"
+print(students[0])
