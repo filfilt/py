5f2209f603daeba20acd1b49051f44fda25502a2
filfilt <tafneg4@gmail.com>
2020-08-06 00:31:03 -0700

    add numbering loop

--- forLoop.py
+++ forLoop.py
@@ -1,6 +1,9 @@
 # student = ["nega","kebede","alemu","chaltu"]
 # for s in student:
 #     print(s)
-name = "nega"
-for x in name:
-    print(x)
+# name = "nega"
+# for x in name:
+#     print(x)
+for n in range(2,16,2):
+    print(n)
+    
