cf1cbcaba241e0506e8eb5bb5152e904a687063b
filfilt <tafneg4@gmail.com>
2020-08-05 23:18:06 -0700

    condition

---
+++ condition.py
@@ -0,0 +1,13 @@
+mark= int(input("Please enter Grade? "))
+if mark>90:
+    grade="A"
+elif mark>80:
+    grade="B"
+elif mark>70:
+    grade="C"
+elif mark>60:
+    grade="D"
+else:
+    grade="F"
+
+print (grade)
