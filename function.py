043dc5b14cd4b061aae4840a3be2d88354f19b1f
filfilt <tafneg4@gmail.com>
2020-08-06 00:55:57 -0700

    commit

---
+++ function.py
@@ -0,0 +1,12 @@
+def sum(n1,n2):
+    return n1+n2
+def diff(n1,n2):
+    return n1-n2
+def product(n1,n2):
+    return n1*n2
+def ques(n1,n2):
+    return n1/n2
+print(sum(12,6))
+print(diff(12,6))
+print(product(12,6))
+print(ques(12,6))
