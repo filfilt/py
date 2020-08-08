e226f6b8d8e7bac4df8eff6159175c3fa3d9fd74
filfilt <tafneg4@gmail.com>
2020-08-08 10:20:47 -0700

    added some explanations

--- osExample.py
+++ osExample.py
@@ -1,9 +1,17 @@
 #To create a folder
 import os
-os.mkdir('week3')
+#os.mkdir('week3')
 # os.mkdir('week4')
 #os.makedirs('Student/Grade/11thGrade')
 
 #To delete a folder
 #os.rmdir('week4')
 #os.rmdir('Student/Grade/9thGrade')
+
+#r=>read, w=>write,x=>delete,a=>appand
+
+f=open('student.txt','r')
+print(f.read())
+
+# f=open('student.txt','a')
+# f.write('Ayele')
